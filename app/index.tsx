d42af2b5f807aa82575a148d64dcaaf0e45a96a0
Roman <romanyunko1996@gmail.com>
2025-12-29 22:50:32 +0200

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import "./globals.css";
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-center"
+    >
+      <Text className="text-5xl font-bold text-primary">Welcome to Movie App</Text>
+    </View>
+  );
+}
